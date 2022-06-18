46b361a69c359525e2e75d2f1bb8e54fd119196d
Gurjashan007 <104344412+Gurjashan007@users.noreply.github.com>
2022-06-18 20:57:02 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,245 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: C:\Users\adity\OneDrive\Desktop\Assignment 5.py ===========
+
+Enter the string to be printed in reverse order :
+computer
+
+The reversed order of string entered is :
+retupmoc
+
+
+Enter the initial number of range :
+5
+
+Enter the last(ending) number of range :
+9
+
+Enter the divisor for entered range :
+3
+
+Numbers divisile in the range are :
+1 . 6
+
+
+Enter the lenght of 1st side of triangle whose area is to be calculated :
+5
+
+Enter the lenght of 2nd side of triangle whose area is to be calculated :
+12
+
+Enter the lenght of 3rd side of triangle whose area is to be calculated :
+13
+
+Area of triangle whose sides are entered is : 30.0
+
+
+
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+enter no of rows to be printed:4
+A
+BC
+DEF
+GHIJ
+Please, Enter the Lowest Range Value: 20
+Please, Enter the Upper Range Value: 80
+The Prime Numbers in the range are: 
+23
+29
+31
+37
+41
+43
+47
+53
+59
+61
+67
+71
+73
+79
+The numbers are divisible by only 7 not 11
+7
+14
+21
+28
+35
+42
+49
+56
+63
+70
+84
+91
+98
+105
+112
+119
+126
+133
+140
+147
+161
+168
+175
+182
+189
+196
+203
+210
+217
+224
+238
+245
+252
+259
+266
+273
+280
+287
+294
+301
+315
+322
+329
+336
+343
+350
+357
+364
+371
+378
+392
+399
+406
+413
+420
+427
+434
+441
+448
+455
+469
+476
+483
+490
+497
+The numbers are only divisible by only 11 not 7
+11
+22
+33
+44
+55
+66
+88
+99
+110
+121
+132
+143
+165
+176
+187
+198
+209
+220
+242
+253
+264
+275
+286
+297
+319
+330
+341
+352
+363
+374
+396
+407
+418
+429
+440
+451
+473
+484
+495
+The numbers are divisible by both 7 and 11
+77
+154
+231
+308
+385
+462
+
+Enter the 1st number to be checked :
+14
+Enter the 2nd number to be checked :
+12
+Enter the 3rd number to be checked :
+30
+Enter the 4th number to be checked :
+54
+Enter the 5th number to be checked :
+-78
+Enter the 6th number to be checked :
+-88
+Enter the 7th number to be checked :
+-25
+Enter the 8th number to be checked :
+1
+Enter the 9th number to be checked :
+44
+Enter the 10th number to be checked :
+852
+
+14 is a positive as well as even number.
+
+12 is a positive as well as even number.
+
+30 is a positive as well as even number.
+
+54 is a positive as well as even number.
+
+-78 is a negative as well as even number.
+
+-88 is a negative as well as even number.
+
+-25 is a negative as well as odd number
+
+1 is a positive as well as odd number
+
+44 is a positive as well as even number.
+
+852 is a positive as well as even number.
+
+The following input numbers are odd :
+ , -25 , 1
+
+The following input numbers are positive :
+ , 14 , 12 , 30 , 54 , 1 , 44 , 852
+
+The following input numbers are negative :
+ , -78 , -88 , -25
+
+The following input numbers are even :
+ , 14 , 12 , 30 , 54 , -78 , -88 , 44 , 852
+
+Enter the number to be checked for number of appearences :
+44
+The number appeared 1 times.
+Enter the statement :
+introduction to computing
+Enter the word whose occurneces are to be counted :
+c
+2
